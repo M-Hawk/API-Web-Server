5ec8ea673b788326234d399b95aa3c78519f6a0a
M-Hawk <12922@coderacademy.edu.au>
2022-11-08 14:36:41 +1000

    added config files (.env, pycache, venv, flaskenv, gitignore, and created basic working Flask app

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app():
+    # using a list comprehension and multiple assignment 
+    # to grab the environment variables we need
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows us to use our ORM
+    db = SQLAlchemy(app)
+    
+    return app
