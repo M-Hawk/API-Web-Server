b9238860a1151296b34ac586c9be3b73dc2b4095
M-Hawk <12922@coderacademy.edu.au>
2022-11-13 01:32:02 +1000

    added auth and validation requirements into main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from controllers.areas_controller import areas_bp
 from controllers.sectors_controller import sectors_bp
 from controllers.problems_controller import problems_bp
 from controllers.ascents_controller import ascents_bp
-# from controllers.climbers_controller import climbers_bp
+from controllers.auth_controller import auth_bp
 from controllers.cli_controller import db_commands
 from marshmallow.exceptions import ValidationError
 import os
@@ -17,7 +17,27 @@ def create_app():
     # Creating the flask app object - this is the core of our app!
     app = Flask(__name__)
 
-    # associating database, marshmallow, bcrypt and jwt objects with our app
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {'error': err.messages}, 400
+
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {'error': str(err)}, 400
+
+    @app.errorhandler(404)
+    def not_found(err):
+        return {'error': str(err)}, 404
+
+    @app.errorhandler(401)
+    def unauthorized(err):
+        return {'error': 'You are not authorized to perform this action'}, 401
+
+    @app.errorhandler(KeyError)
+    def key_error(err):
+        return {'error': f'The field {err} is required.'}, 400
+
+    
 
 
     # import the controllers and activate the blueprints
@@ -26,10 +46,14 @@ def create_app():
     # app.register_blueprint(auth_bp)
 
     # prevents dict keys lists from auto ordering by alphabet and follows marshmallow ordering
-    app.config ['JSON_SORT_KEYS'] = False
+    app.config ["JSON_SORT_KEYS"] = False
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
+    # allows the use of JWT secret keys in the application
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    # associating database, marshmallow, bcrypt and jwt objects with our app
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
@@ -42,7 +66,7 @@ def create_app():
     app.register_blueprint(sectors_bp)
     app.register_blueprint(problems_bp)
     app.register_blueprint(ascents_bp)
-    # app.register_blueprint(states_bp)
+    app.register_blueprint(auth_bp)
 
     return app
 
